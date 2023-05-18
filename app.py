0e6127e199554c561b0ef7d52e9e937e3f244479
@mwombeki <hackerlubex@protonmail.com>
2023-05-18 21:57:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ st.markdown("Source of the dataset: [Click Here](https://www.narcis.nl/dataset/R
 
 st.subheader("🧭 Problem Statement:")
 st.text("""The target feature is Accident_severity which is a multi-class variable. 
-The task is to classify this variable based on the other 31 features step-by-step by going through each day's task. 
+The task is to classify this variable based on the other 31 features step-by-step by going through each days task. 
 The metric for evaluation will be f1-score
 """)
 
